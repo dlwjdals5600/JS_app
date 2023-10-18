59122d6664b2b7932394eb484fed0c16dffd7c5f
이정민 <dlwjdals5600@gmail.com>
2023-10-18 19:19:15 +0900

    날씨정보 알려주기 기능 추가

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+const API_KEY = ""
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main}`
+        });
+}
+
+function onGeoError() {
+    alert("날씨를 찾지 못했습니다.")
+}
+
+
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
